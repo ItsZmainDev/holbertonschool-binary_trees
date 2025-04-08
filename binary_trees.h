000404e1a4e717cfcd68ea45cc1192c8e845fa34
Terencetjt26 <10597@holbertonstudents.com>
2025-04-08 17:37:49 +0000

    add function to check if a binary tree node is a leaf

--- binary_trees.h
+++ binary_trees.h
@@ -26,6 +26,7 @@ typedef struct binary_tree_s binary_tree_t;
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+int binary_tree_is_leaf(const binary_tree_t *node);
 
 /* Optionally include binary_tree_print if you use it */
 void binary_tree_print(const binary_tree_t *);
